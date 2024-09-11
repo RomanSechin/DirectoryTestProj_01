25fb8aa65c808a479e34f205e96a0c565d521805
Roman Sechin <romansechin@gmail.com>
2024-09-11 16:58:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    //foreach (string directory in Directory.GetDirectories("C:\\"))
+    //  Console.WriteLine(directory);
+    //foreach (string file in Directory.EnumerateFiles("F:\\"))
+    //  Console.WriteLine(file);
+    foreach (string еntrie in Directory.GetFileSystemEntries("C:\\"))
+      Console.WriteLine(еntrie);
+  }
+}
